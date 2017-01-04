5d43ee33f41221cf7bac1ef967c3be6fd68a0d6a
Shree Harsha Sridharamurthy <shreeharshas@users.noreply.github.com>
2017-01-04 00:32:30 -0500

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 
 
-class mystack{
+class my_stack{
 int stack[100];
-int top =-1;
+int top;
+
+public: my_stack(){
+    top = -1;
+}
 
 bool push(int n){
     top++;
@@ -21,30 +25,51 @@ bool push(int n){
 
 int pop(bool *is_empty){
     if(top==-1){
-        is_empty=true;
+        *is_empty=true;
         return -1;
     }
     else{
-        is_empty = false;
-        return this.stack[this.top--];
+        *is_empty = false;
+        return stack[top--];
     }    
 }
 
 int peek(bool *is_empty){
     if(top==-1){
-        is_empty==true;
+        *is_empty=true;
         return -1;
     }
     else{
-        is_empty = false;
+        *is_empty = false;
         return stack[top];
     }
 }
-} my_stack;
+};
+
 int main() {
-	my_stack a = new my_stack();
-	a.push(10);
-	printf("Stack top:%d", a.peek());
-	a.pop();
+	my_stack *a = new my_stack;
+	int val = 10;
+	bool is_pushed = a->push(val);
+	if(is_pushed){
+	    cout<<"Successfully pushed:"<<val<<endl;
+	}
+	
+	bool *is_peek = new bool;
+	int peek_val = a->peek(is_peek);
+	if(is_peek){
+	    cout<<"Stack top:"<<peek_val<<endl;
+	}
+	else{
+	    cout<<"Stack Empty"<<endl;
+	}
+	
+	bool *is_pop = new bool;
+	int x = a->pop(is_pop);
+	if(*is_pop){
+	    cout<<"Popped value:"<<x<<endl;
+	}
+	else{
+	    cout<<"Stack Empty"<<endl;
+	}
 	return 0;
 }
